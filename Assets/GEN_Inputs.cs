ecaa18998a1bb743ae14490d69f46d8eca0916b9
JoaquinDavinci <joaquin.inofuentes@davinci.edu.ar>
2026-04-24 22:43:16 -0300

    1

---
+++ Assets/GEN_Inputs.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GEN_Inputs : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
